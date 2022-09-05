0be8fa67bb234b3c158d44bbb64cf1b1d8f6d710
chrscmpl <chcampolongo1@gmail.com>
2022-09-05 10:46:10 +0200

    Add webpack merge, multiple configurations, css minimizer

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	devtool: 'inline-source-map',
 	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -16,10 +15,6 @@ module.exports = {
 	],
 	module: {
 		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /.s?css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+		],
+	},
+	optimization: {
+		minimizer: [new CssMinimizerPlugin()],
+	},
+	plugins: [new MiniCssExtractPlugin()],
+});
