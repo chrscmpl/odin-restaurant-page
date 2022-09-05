185d198996b1fa9e1842f24a1b32c24999c8e8b3
chrscmpl <chcampolongo1@gmail.com>
2022-09-05 23:23:43 +0200

    Style adjustments

--- src/menu/menu-section/menu-item/images.js
+++ src/menu/menu-section/menu-item/images.js
@@ -1,11 +1,15 @@
 import margherita from './images/margherita.jpg';
+import mimosa from './images/mimosa.jpg';
 
 const Gallery = {
 	getImgPath(name) {
 		const image = this.images.find(img => img.name === name.toLowerCase());
 		return image ? image.path : '';
 	},
-	images: [{ name: 'margherita', path: margherita }],
+	images: [
+		{ name: 'margherita', path: margherita },
+		{ name: 'mimosa', path: mimosa },
+	],
 };
 
 export default Gallery;
