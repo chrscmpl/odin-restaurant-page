9005a99228d8fc76514d914a4b84778ecabec01b
chrscmpl <chcampolongo1@gmail.com>
2022-09-05 21:53:25 +0200

    Add image minizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -14,7 +15,22 @@ module.exports = merge(common, {
 		],
 	},
 	optimization: {
-		minimizer: [new CssMinimizerPlugin(), '...'],
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.squooshMinify,
+					options: {
+						encodeOptions: {
+							mozjpeg: {
+								quality: 60,
+							},
+						},
+					},
+				},
+			}),
+			'...',
+		],
 	},
 	plugins: [new MiniCssExtractPlugin()],
 });
